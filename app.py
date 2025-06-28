f28280de96eaca9e80805e7b26400f5c9d12a722
rpacheco-1996 <rgp1996@yahoo.com>
2025-06-28 00:47:21 -0700

    add legacy

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask on Render!"
 
+@app.route('/legacy/')
+def legacy():
+    return "HELLO LEGACY!!!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
