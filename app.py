d096f86d36d567543daccaaeea4ae7b2998a611d
rpacheco-1996 <rgp1996@yahoo.com>
2025-06-28 01:13:31 -0700

    add cors functionality

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+import pandas as pd
+import psycopg2
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return "Hello from Flask on Render!"
 
-@app.route('/legacy/')
+@app.route('/legacy')
 def legacy():
-    return "HELLO LEGACY!!!"
+    user_input = request.args.get('input', '')
+    return f"Received input: {user_input}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
