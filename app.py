b997abf56117a024d19140f3742ad94d31b60b1c
rpacheco-1996 <rgp1996@yahoo.com>
2025-06-28 01:36:49 -0700

    add db functionallity

--- app.py
+++ app.py
@@ -6,6 +6,38 @@ import psycopg2
 app = Flask(__name__)
 CORS(app)
 
+def query(keyword):
+    DB_NAME = "legacy_pacheco"
+    DB_USER = "rpacheco"
+    DB_PASSWORD = "D1qoGvjXqPTd2vB4i4KS71xDEWmCOpSq"
+    DB_HOST = "dpg-d1ff63mmcj7s739p963g-a.oregon-postgres.render.com"
+    DB_PORT = "5432"
+
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        sslmode="require"
+    )
+    cursor = conn.cursor()
+
+    query = f"select response from records where '{keyword}' = ANY(keywords)"
+
+    cursor.execute(query, (True,))
+    # Fetch all rows from the executed query
+    rows = cursor.fetchall()
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    if len(rows) > 0:
+        return rows[0][0]
+    else:
+        return "No related response found"
+
+
 @app.route('/')
 def home():
     return "Hello from Flask on Render!"
@@ -13,7 +45,8 @@ def home():
 @app.route('/legacy')
 def legacy():
     user_input = request.args.get('input', '')
-    return f"Received input: {user_input}"
+    response = query(user_input)
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
