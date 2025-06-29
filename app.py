52eb299f196cf9e26baf8992fb6a6b4be4644e86
rpacheco-1996 <rgp1996@yahoo.com>
2025-06-28 23:13:32 -0700

    add list selection

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 import psycopg2
@@ -7,7 +7,7 @@ import random
 app = Flask(__name__)
 CORS(app)
 
-def query(keyword):
+def query(query):
     DB_NAME = "legacy_pacheco"
     DB_USER = "rpacheco"
     DB_PASSWORD = "D1qoGvjXqPTd2vB4i4KS71xDEWmCOpSq"
@@ -24,8 +24,6 @@ def query(keyword):
     )
     cursor = conn.cursor()
 
-    query = f"select response from records where '{keyword}' = ANY(keywords)"
-
     cursor.execute(query, (True,))
     # Fetch all rows from the executed query
     rows = cursor.fetchall()
@@ -33,10 +31,7 @@ def query(keyword):
     cursor.close()
     conn.close()
 
-    if len(rows) > 0:
-        return rows[int(random.randint(0, len(rows)))][0]
-    else:
-        return "No related response found"
+    return rows
 
 
 @app.route('/')
@@ -46,8 +41,33 @@ def home():
 @app.route('/legacy')
 def legacy():
     user_input = request.args.get('input', '')
-    response = query(user_input)
-    return response
+    query_string = f"select response from records where '{user_input}' = ANY(keywords)"
+    rows = query(query_string)
+    if len(rows) > 0:
+        return rows[int(random.randint(0, len(rows)))][0]
+    else:
+        return "No related response found"
+
+@app.route('/getClinics')
+def get_clinics():
+    query_string = "select distinct clinic from records order by clinic"
+    rows = query(query_string)
+    if len(rows) > 0:
+        clinics = [row[0] for row in rows]
+        return jsonify(clinics)
+    else:
+        return jsonify(["No clinics found"])
+    
+@app.route('/getDoctors')
+def get_clinics():
+    query_string = "select distinct doctor from records order by doctor"
+    rows = query(query_string)
+    if len(rows) > 0:
+        doctors = [row[0] for row in rows]
+        return jsonify(doctors)
+    else:
+        return jsonify(["No doctors found"])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
